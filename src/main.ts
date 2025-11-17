a61e883498090c3fe4e96f5a4bf2481cce0d5d96
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-17 12:59:25 +0700

    generating a few files

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(ComputerModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
