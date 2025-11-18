39d57621393ba212aae041c5eece5ac9268cb4fb
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-18 17:42:43 +0700

    revert to App setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ComputerModule } from './computer/computer.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ComputerModule);
-
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
