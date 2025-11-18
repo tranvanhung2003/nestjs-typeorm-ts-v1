b339b8ca17942dac89f4204dccd1c14e78e92ed3
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-18 11:13:49 +0700

    consuming multiple modules

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -3,5 +3,11 @@ import { PowerService } from 'src/power/power.service';
 
 @Injectable()
 export class CpuService {
-  constructor(private readonly powerService: PowerService) {}
+  constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from PowerService');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
+import { DiskService } from './disk.service';
 
-@Module({})
+@Module({
+  imports: [PowerService],
+  providers: [DiskService],
+})
 export class DiskModule {}
