b546ea28a63d29a3770b48c0c591f479341eeb01
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-20 10:24:22 +0700

    creating and saving a user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return await this.repo.save(user);
+  }
+}
