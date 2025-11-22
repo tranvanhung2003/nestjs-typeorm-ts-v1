906820c127bc8ec2bf0f5bae5d2791d1c20b66d5
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-22 21:11:34 +0700

    how to build interceptors

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,20 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    // Run something before a request is handled
+    // by the request handler
+    console.log("I'm running before the handler", context);
+
+    return next.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        console.log("I'm running before the response is sent out", data);
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import {
   Body,
-  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -11,6 +10,7 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -25,7 +25,7 @@ export class UsersController {
   }
 
   @Get(':id')
-  @UseInterceptors(ClassSerializerInterceptor)
+  @UseInterceptors(SerializeInterceptor)
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
 
