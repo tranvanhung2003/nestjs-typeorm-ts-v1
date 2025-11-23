96f7f248d1c7987cf45ef027218fa74df3d81c2b
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-23 23:44:18 +0700

    salting and hashing the password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { scrypt as _scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -12,7 +16,16 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('Email in use');
     }
+
     // Hash the users password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed result and the salt together
+    const result = `${salt}.${hash.toString('hex')}`;
     // Create a new user and save it
     // Return the user
   }
