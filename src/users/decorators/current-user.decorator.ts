ca30da4ba037776e034ebae0621d37c424ce97c0
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-29 11:46:16 +0700

    communicating from interceptor to decorator

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -2,8 +2,10 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: never, ctx: ExecutionContext) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const request = ctx.switchToHttp().getRequest();
-    console.log(request.session.userId);
-    return 'hi there';
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    return request.currentUser;
   },
 );
---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,24 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from '../users.service';
+
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private readonly usersService: UsersService) {}
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Promise<Observable<any>> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const request = context.switchToHttp().getRequest();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const { userId } = request.session;
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      request.currentUser = user;
+    }
+
+    return next.handle();
+  }
+}
